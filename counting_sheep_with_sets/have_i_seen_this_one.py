8d0629a1b1cafd203ee65ef65075526b4bda11f7
Simon Prickett <simon@redislabs.com>
2021-04-29 17:03:26 -0700

    Adds counting sheep examples.

---
+++ counting_sheep_with_sets/have_i_seen_this_one.py
@@ -0,0 +1,10 @@
+sheep_seen = { "1934", "1201", "1199", "0007", "3409", "1015" }
+
+def have_i_seen(sheep_id):
+    if sheep_id in sheep_seen:
+        print(f"I have seen sheep {sheep_id}.")
+    else:
+        print(f"I have not seen sheep {sheep_id}.")
+
+have_i_seen("1934")
+have_i_seen("1283")
---
+++ counting_sheep_with_sets/how_many.py
@@ -0,0 +1,11 @@
+sheep_seen = set()
+
+sheep_seen.add("1934")
+sheep_seen.add("1201")
+sheep_seen.add("1199")
+sheep_seen.add("0007")
+sheep_seen.add("3409")
+sheep_seen.add("1934")
+sheep_seen.add("1015")
+
+print(f"There are {len(sheep_seen)} sheep.")
